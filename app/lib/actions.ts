7d29f9ed9bf9e91ba388262bbd5aab246bfd8b86
rajaappala <rajashekarappala@gmail.com>
2025-05-24 13:16:06 +0530

    resolved lint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -109,6 +109,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
     } catch (error) {
+        console.log(error);
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -50,6 +50,14 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         {/* Invoice Amount */}
