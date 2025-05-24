e350c7cf18bbbd8de6df5132c2a03d140aea332e
rajaappala <rajashekarappala@gmail.com>
2025-05-24 13:19:28 +0530

    resolved lint error

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,8 +1,8 @@
 // import bcrypt from 'bcryptjs';
-// import postgres from 'postgres';
+import postgres from 'postgres';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 // async function seedUsers() {
 //   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -101,17 +101,18 @@
 //   return insertedRevenue;
 // }
 
-// export async function GET() {
-//   try {
-//     const result = await sql.begin((sql) => [
-//       // seedUsers(),
-//       // seedCustomers(),
-//       // seedInvoices(),
-//       // seedRevenue(),
-//     ]);
+export async function GET() {
+  try {
+    await sql.begin((sql) => [
+      // seedUsers(),
+      // seedCustomers(),
+      // seedInvoices(),
+      // seedRevenue(),
+      console.log(sql)
+    ]);
 
-//     return Response.json({ message: 'Database seeded successfully' });
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
+    return Response.json({ message: 'Database seeded successfully' });
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
